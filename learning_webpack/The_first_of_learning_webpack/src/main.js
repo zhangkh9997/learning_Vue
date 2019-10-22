3ca0e98e57baccd2d1f31fa3d1358391158426e2
zhangkh9997 <zhangkh9997@163.com>
2019-10-22 21:24:47 +0800

    learning webpack first time

---
+++ learning_webpack/The_first_of_learning_webpack/src/main.js
@@ -0,0 +1,12 @@
+// 这是整个程序的入口文件
+
+// 使用commonJS的模块化规范导入
+const {add, mul} = require('./mathUtils');
+
+// 这是使用ES6的模块化规范导入
+import {name, age, height} from "./info.js";
+
+console.log(add(10, 20));
+console.log(mul(10, 20));
+
+console.log(name, age, height);
