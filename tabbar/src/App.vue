f6449baaa757adaf3de6f3b60eb6f21bb504ebff
zhangkh9997 <zhangkh9997@163.com>
2019-10-31 15:51:21 +0800

    封装tabbar组件

---
+++ tabbar/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from "./components/maintabbar/MainTabBar";
+  export default {
+    name: 'App',
+    components:{
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  /*在style中引用文件的固定格式，使用 @import 开头*/
+  @import "assets/css/base.css";
+
+</style>
